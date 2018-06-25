233140c2ea45ea946059c8d576dbf9d424dcd91f
Aleksei <ykt.toshi@gmail.com>
2018-06-25 23:20:48 +1200

    Lesson 7 task 1

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,24 +1,9 @@
 class ClientsController < ApplicationController
-  before_action :set_client, only: [:show, :edit, :update, :destroy]
-
-  # GET /clients
-  # GET /clients.json
-  def index
-    @clients = Client.all
-  end
-
-  # GET /clients/1
-  # GET /clients/1.json
-  def show
-  end
+  layout 'landing' 
 
   # GET /clients/new
   def new
     @client = Client.new
-  end
-
-  # GET /clients/1/edit
-  def edit
   end
 
   # POST /clients
@@ -28,7 +13,7 @@ class ClientsController < ApplicationController
 
     respond_to do |format|
       if @client.save
-        format.html { redirect_to @client, notice: 'Client was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Client was successfully created.' }
         format.json { render :show, status: :created, location: @client }
       else
         format.html { render :new }
@@ -37,35 +22,7 @@ class ClientsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /clients/1
-  # PATCH/PUT /clients/1.json
-  def update
-    respond_to do |format|
-      if @client.update(client_params)
-        format.html { redirect_to @client, notice: 'Client was successfully updated.' }
-        format.json { render :show, status: :ok, location: @client }
-      else
-        format.html { render :edit }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /clients/1
-  # DELETE /clients/1.json
-  def destroy
-    @client.destroy
-    respond_to do |format|
-      format.html { redirect_to clients_url, notice: 'Client was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_client
-      @client = Client.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def client_params
